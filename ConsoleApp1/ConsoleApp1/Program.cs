7d020c1c1be9943e18eee67ec538a94d3ac9c8dd
markforest <toxerius7@gmail.com>
2018-10-03 20:15:29 +0300

    commit1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(JobForThread);
+            ThreadPool.QueueUserWorkItem(JobForThread2);
+            ThreadPool.QueueUserWorkItem(JobForThread3);
+            //JobForThread("f");
+            //JobForThread2("f");
+            //JobForThread3("f");
+            Console.WriteLine("I am not wait!! I doing");
+            Console.ReadKey();
+
+        }
+
+        private static void JobForThread(object state)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Цыкл {i}, выополняется внутри потока из пула {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(20);
+            }
+        }
+
+        private static void JobForThread2(object state)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"\tЦыкл {i}, выополняется внутри потока из пула {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(20);
+            }
+        }
+
+
+        private static void JobForThread3(object state)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"\t\tЦыкл {i}, выополняется внутри потока из пула {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(20);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var item in Controls)
+            {
+                if (item is PictureBox)
+                {
+                    (item as PictureBox).Image = Image.FromFile("1.png");
+                    (item as PictureBox).SizeMode = PictureBoxSizeMode.Zoom;
+                }
+            }
+        }
+
+        private void rocketStart1(object j)
+        {
+            List<PictureBox> lists = new List<PictureBox>();
+            foreach (var item in Controls)
+            {
+                if(item is PictureBox)
+                {
+                    lists.Add((item as PictureBox));
+                }
+            }
+            
+
+            while (true)
+            {
+                PictureBox p = lists[r.Next(lists.Count)];
+                
+                if (p.Location.Y < Bounds.Top-100)
+                {
+                    p.Invoke(new Action(() => {
+                        p.Image = Image.FromFile("2.png");
+                    }));
+                    //break;
+                }
+                p.Invoke(new Action(()=> {
+                    p.Location = new Point(p.Location.X, p.Location.Y - r.Next(10));
+
+                }));
+                Thread.Sleep(r.Next(20));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(rocketStart1);
+        }
+    }
+}
